f2a16aea710af19153f0a1c18020b496f39ba25c
Ng Wei Xiang <weixng97@gmail.com>
2024-11-26 11:33:22 +0800

    Design navbar and sidenav and menu items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LayoutComponent, children: [
+        { path: '', redirectTo: 'menu', pathMatch: 'full' },
+        { path: 'menu', component: MenuComponent }
+    ] }
+];
---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+  menuList = [
+    { id: 'Menu1', link: '/menu1', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
+    { id: 'Menu2', link: '/menu2', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
+    { id: 'Menu3', link: '/menu3', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
+  ];
+
+  ngOnInit() 
+  {
+    console.log(this.menuList);
+  }
+}
