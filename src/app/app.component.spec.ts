1c2f5f826bbe8463b445e0720282f1e6f3ddf9cf
Ng Wei Xiang <weixng97@gmail.com>
2024-11-26 17:25:47 +0800

    Change content in readme.md

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'simpleweb01' title`, () => {
+  it(`should have the 'myerp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simpleweb01');
+    expect(app.title).toEqual('myerp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, simpleweb01');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myerp');
   });
 });
