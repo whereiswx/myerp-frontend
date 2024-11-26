1737af62d8f7ce4929380a1cff4ad9e019212d9c
Ng Wei Xiang <weixng97@gmail.com>
2024-11-26 17:37:48 +0800

    Change title in index.html, Change icon for info

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -10,9 +10,9 @@ import { RouterLink } from '@angular/router';
 })
 export class MenuComponent {
   menuList = [
-    { id: 'Menu1', link: '/menu1', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
-    { id: 'Menu2', link: '/menu2', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
-    { id: 'Menu3', link: '/menu3', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-info' },
+    { id: 'Menu1', link: '/menu1', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-circle-info' },
+    { id: 'Menu2', link: '/menu2', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-circle-info' },
+    { id: 'Menu3', link: '/menu3', bg: 'bg-primary btn text-white', faicon: 'fa fa-fw fa-circle-info' },
   ];
 
   ngOnInit() 
